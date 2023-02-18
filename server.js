171fd7fd2a527a32ffb4f048e571b9f120639191
edigaryannn <gor.edigaryan@tumo.org>
2023-02-18 12:39:15 +0400

    less4

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 var express = require("express");
 var app = express();
-app.get("/", function (req, res) {
-    res.send("<h1>Hello world</h1>");
+app.use(express.static("modules"));
+app.get("/", function(req, res){
+    res.redirect("index.html");
 });
-app.get("/name/:name", function (req, res) {
-    var name = req.params.name;
-    res.send("<h1>Hello " + name + "</h1>");
-});
-app.listen(3000, function () {
+app.listen(3000, function(){
     console.log("Example is running on port 3000");
 });
