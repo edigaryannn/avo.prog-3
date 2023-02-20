32292d40b9869697b762481756e5e3bef391748c
edigaryannn <gor.edigaryan@tumo.org>
2023-02-20 14:27:55 +0400

    ls4 home

--- test/server.js
+++ test/server.js
@@ -1,13 +1,23 @@
-var fs = require('fs');
-var dummyText = "Apple yep";
-function main() {
-    fs.writeFileSync("dummytext.txt", dummyText);
-    var text = fs.readFileSync("dummytext.txt").toString();
-    console.log(dummyText == text)
-    console.log(text)
-    fs.writeFileSync("undummytext.txt",
-        text.replace("Apple", "Microsoft")
-    );
-}
-
-main();
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var messages = [];
+app.use(express.static("."));
+app.get('/', function (req, res) {
+    res.redirect('index.html');
+    console.log("Example is running on port 3000");
+})
+server.listen(3000, function () {
+    console.log("Example is running on port 3000");
+});
+io.on('connection', function (socket) {
+    for (var i in messages) {
+        socket.emit("display message", messages[i]);
+    }
+    socket.on("send message", function (data) {
+        messages.push(data);
+        io.sockets.emit("display message", data);
+    });
+});
+console.log(messages);
